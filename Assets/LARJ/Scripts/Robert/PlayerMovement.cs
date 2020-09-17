b6a3fd349756fd94a80715c95aeabd344ce16093
Donschnitzel <58332117+Donschnitzel@users.noreply.github.com>
2020-09-17 16:39:50 +0200

    Implemented Dash

--- Assets/LARJ/Scripts/Robert/PlayerMovement.cs
+++ Assets/LARJ/Scripts/Robert/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,11 +9,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed = 3;
     [SerializeField] private int _playerIndex = 0;
+    [SerializeField] private float _dashCooldown = 2f;
+    [SerializeField] private float _dashDistance = 2f;
 
     private CharacterController _controller;
 
     private Vector3 _moveDirection = Vector3.zero;
     private Vector2 _inputVector = Vector2.zero;
+    private float _dashTimer = 0f;
+    private bool _isDashOnCooldown = false;
+    private float _dashSpeed = 10;
+    private bool _inDash = false;
 
     private void Awake()
     {
@@ -25,11 +32,28 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(InputValue inputValue)
     {
         _inputVector = inputValue.Get<Vector2>();
-       
+
     }
     private void Update()
     {
-        Move();
+        if (!_inDash)
+        {
+            Move();
+        }
+        UpdateDashCooldown();
+    }
+
+    private void UpdateDashCooldown()
+    {
+        if (_isDashOnCooldown)
+        {
+            _dashTimer += Time.deltaTime;
+            if (_dashTimer >= _dashCooldown)
+            {
+                _isDashOnCooldown = false;
+                _dashTimer = 0;
+            }
+        }
     }
 
     private void Move()
@@ -37,5 +61,34 @@ public class PlayerMovement : MonoBehaviour
         _moveDirection = new Vector3(_inputVector.x, 0, _inputVector.y);
         _moveDirection = transform.TransformDirection(_moveDirection);
         _controller.Move(_moveDirection * _movementSpeed * Time.deltaTime);
+    }
+    private void OnDash(InputValue inputValue)
+    {
+        if (inputValue.isPressed && !_isDashOnCooldown)
+        {
+            StartCoroutine(Dash());
+            //Dash();
+        }
+    }
+    IEnumerator Dash()
+    {
+
+        float i = 0;
+        _inDash = true;
+        while (i <= 1)
+        {
+
+            _controller.Move(_moveDirection * _dashDistance * Time.deltaTime);
+            i += Time.deltaTime * _dashSpeed;
+            yield return null;
+        }
+        _inDash = false;
+        yield return null;
+    }
+    private void Daxsh()
+    {
+        _dashTimer = 0f;
+        _isDashOnCooldown = true;
+        _controller.Move(_moveDirection * _dashDistance);
     }
 }
