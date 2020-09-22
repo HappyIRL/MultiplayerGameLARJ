8ac052d4dfa4a67778ba74a0f5042af58e18a31b
Donschnitzel <58332117+Donschnitzel@users.noreply.github.com>
2020-09-22 20:37:07 +0200

    Started working on TaskManager

--- Assets/LARJ/Scripts/Robert/TaskManager.cs
+++ Assets/LARJ/Scripts/Robert/TaskManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Tasks
@@ -12,9 +13,18 @@ namespace Tasks
         [SerializeField] private int _openTasks;
         [SerializeField] private Task[] _possibleTasks;
         [SerializeField] private float _delayBetweenTasks;
+        [SerializeField] private TextMeshProUGUI[] _tasksListText;
+        [SerializeField] private TextMeshProUGUI _scoreText;
         float _timer = 0f;
+        private byte[] _taskListCounter = new byte[(int)TaskType.NotAssigned];
 
-        // Update is called once per frame
+        private void Awake()
+        {
+            for (int i = 0; i < _taskListCounter.Length; i++)
+            {
+                _taskListCounter[i] = 0;
+            }
+        }
         void Update()
         {
             _timer += Time.deltaTime;
@@ -28,16 +38,90 @@ namespace Tasks
         private void EnableTask()
         {
             int i = UnityEngine.Random.Range(0, _possibleTasks.Length);
-            _possibleTasks[i].enabled = true;
+            Task task = _possibleTasks[i];
+            task.enabled = true;
+            UpdateTasksText((int)task.GetTaskType,true);
         }
 
-        public void OnTaskCompleted()
+
+        /// <summary>
+        /// Updates the TasksTExt in  the UI, taskType decides which Taskstext should be modified and positive means eigther increasing the Taskcounter or decreasing it.
+        /// </summary>
+        /// <param name="taskType"></param>
+        /// <param name="positive"></param>
+        private void UpdateTasksText(int taskType, bool positive)
+        {
+            if (positive)
+            {
+                _taskListCounter[taskType]++;
+                switch (taskType)
+                {
+                    case 0:
+                        _tasksListText[taskType].text = _taskListCounter[taskType] + " Answer Phone Call!";
+                        break;
+                    case 1:
+                        _tasksListText[taskType].text = _taskListCounter[taskType] + " Print Documents!!";
+                        break;
+                    case 2:
+                        _tasksListText[taskType].text = _taskListCounter[taskType] + " Talk to Customer!!!";
+                        break;
+                    //Add more if we have more Tasks
+                    default:
+                        Debug.LogError("Task is missing TaskType");
+                        break;
+                }
+            }
+            else
+            {
+                _taskListCounter[taskType]--;
+                if (_taskListCounter[taskType] > 0)
+                {
+                    switch (taskType)
+                    {
+                        case 0:
+                            _tasksListText[taskType].text = _taskListCounter[taskType] + " Answer Phone Call!";
+                            break;
+                        case 1:
+                            _tasksListText[taskType].text = _taskListCounter[taskType] + " Print Documents!!";
+                            break;
+                        case 2:
+                            _tasksListText[taskType].text = _taskListCounter[taskType] + " Talk to Customer!!!";
+                            break;
+                        //Add more if we have more Tasks
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    if (_taskListCounter[taskType] < 0)
+                    {
+                        Debug.LogError("Try to delete a Task, that was never given.");
+                        _taskListCounter[taskType] = 0;
+                    }
+                    _tasksListText[taskType].text = "";
+                }
+            }
+
+        }
+
+        public void OnTaskCompleted(TaskType taskType)
         {
             _completedTasks++;
+            UpdateTasksText((int)taskType, false);
+            UpdateScore();
         }
-        public void OnTaskFailed()
+
+        private void UpdateScore()
+        {
+            _scoreText.text = (_completedTasks * 20 - _failedTasks * 10).ToString();
+        }
+
+        public void OnTaskFailed(TaskType taskType)
         {
             _failedTasks++;
+            UpdateTasksText((int)taskType, false);
+            UpdateScore();
         }
     }
 }
