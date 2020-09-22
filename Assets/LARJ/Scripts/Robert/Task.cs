139d4d602110c323f5a8bb4825cbde218ee7b626
Donschnitzel <58332117+Donschnitzel@users.noreply.github.com>
2020-09-22 15:40:55 +0200

    Started Working on Tasks

---
+++ Assets/LARJ/Scripts/Robert/Task.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    public int CountDown { get; set; }
+    public bool IsTaskFinished { get; set; }
+
+    public Task FollowUpTask { get; set; }
+
+}
---
+++ Assets/LARJ/Scripts/Robert/TaskManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tasks
+{
+    public class TaskManager : MonoBehaviour
+    {
+        [SerializeField] private int _completedTasks = 0;
+        [SerializeField] private int _failedTasks;
+        [SerializeField] private int _openTasks;
+        [SerializeField] private Task[] _possibleTasks;
+        [SerializeField] private float _delayBetweenTasks;
+        float _timer = 0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= _delayBetweenTasks)
+            {
+                _timer = 0f;
+                EnableTask();
+            }
+        }
+
+        private void EnableTask()
+        {
+            int i = UnityEngine.Random.Range(0, _possibleTasks.Length);
+            _possibleTasks[i].enabled = true;
+        }
+
+        public void OnTaskCompleted()
+        {
+            _completedTasks++;
+        }
+        public void OnTaskFailed()
+        {
+            _failedTasks++;
+        }
+    }
+}
