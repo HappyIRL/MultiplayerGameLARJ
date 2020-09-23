eba5e1765a2853975c0c7d5c1934509ebdaf3da3
Luca <luca.beisser@gmx.de>
2020-09-23 12:13:08 +0200

    Interactables UI Buttons fix
    & put scripts in subfolders

---
+++ Assets/LARJ/Scripts/Luca/UI/DontRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontRotate : MonoBehaviour
+{
+    [SerializeField] private float _yOffset;
+    [SerializeField] private Transform _parent;
+
+    private void LateUpdate()
+    {
+        transform.position = _parent.position + Vector3.up * _yOffset;
+    }
+}
