3769433adca132a91172cd257a888be4ff272191
Luca <luca.beisser@gmx.de>
2020-09-28 21:50:59 +0200

    Added Buttons to UI

---
+++ Assets/LARJ/Scripts/MainMenu/MenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField] private AudioClip _buttonClickSound = null;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _buttonClickSound;
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Click");
+        _audioSource.Play();
+        Application.Quit();
+    }
+}
