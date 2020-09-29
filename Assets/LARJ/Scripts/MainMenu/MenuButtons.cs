b801d4caf118b8dc84697a70ad7871ef44c946b4
Luca <luca.beisser@gmx.de>
2020-09-29 12:03:18 +0200

    Added SceneChanger

--- Assets/LARJ/Scripts/MainMenu/MenuButtons.cs
+++ Assets/LARJ/Scripts/MainMenu/MenuButtons.cs
@@ -1,23 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(AudioSource))]
 public class MenuButtons : MonoBehaviour
 {
     [SerializeField] private AudioClip _buttonClickSound = null;
+    [SerializeField] private SceneChanger _sceneChanger = null;
+
+    [Header("Screens")]
+    [SerializeField] private GameObject _mainMenuScreen = null;
+    [SerializeField] private GameObject _startPlayScreen = null;
     private AudioSource _audioSource;
 
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
         _audioSource.clip = _buttonClickSound;
+
+        OpenMainMenuScreen();
     }
 
+    public void PlayButtonClickSound()
+    {
+        _audioSource.Play();
+    }
     public void ExitGame()
     {
-        Debug.Log("Click");
-        _audioSource.Play();
         Application.Quit();
+    }
+    public void OpenMainMenuScreen()
+    {
+        _mainMenuScreen.SetActive(true);
+        _startPlayScreen.SetActive(false);
+    }
+    public void OpenStartPlayScreen()
+    {
+        _mainMenuScreen.SetActive(false);
+        _startPlayScreen.SetActive(true);
+    }
+
+    public void StartLocalGame()
+    {
+        //TODo: Fade to play Scene
+        //_sceneChanger.FadeToScene();
     }
 }
---
+++ Assets/LARJ/Scripts/SceneChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//This script changes to the next scene with a fading animation.
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] private Animator _animator = null;
+
+    private int _sceneToLoad;
+ 
+
+    public void FadeToScene(int sceneIndex)
+    {
+        _sceneToLoad = sceneIndex;
+        _animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(_sceneToLoad);
+    }
+}
