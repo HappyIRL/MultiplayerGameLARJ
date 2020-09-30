4bab9d75128e8b3c641e3406d50e47d8df2e3f36
Luca <luca.beisser@gmx.de>
2020-09-30 13:50:48 +0200

    Added TaskManagerUI
    - is responsible for spawning the taskUI prefab and removing it
    - Added timebar on taskUI

---
+++ Assets/LARJ/Scripts/Managers/TaskManagerUI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManagerUI : MonoBehaviour
+{
+    [SerializeField] private ObjectPool _taskUIPool = null;
+    [SerializeField] private Transform _parentCanvas = null;
+    [SerializeField] private Transform _taskSpawnPoint = null;
+    [SerializeField] private Transform _firstTaskPoint = null;
+    [SerializeField] private float _spaceBetweenTasks = 10f;
+
+    private List<TaskUI> _activeUITasks = new List<TaskUI>();
+
+    public void SpawnUITask(string taskName, string rewardText, float timeToCompleteTask)
+    {
+        GameObject obj = _taskUIPool.GetObject();
+        TaskUI task = obj.GetComponent<TaskUI>();
+
+        task.TaskTitle = taskName;
+        task.TaskRewardText = rewardText;
+        task.TimeToComplete = timeToCompleteTask;
+
+        obj.transform.parent = _parentCanvas;
+        obj.transform.position = _taskSpawnPoint.position;
+        _activeUITasks.Add(task);
+
+        AlignTaskUI();
+    }
+
+    public void RemoveUITask(TaskUI taskUI)
+    {
+        if (_activeUITasks.Contains(taskUI))
+        {
+            //remove from list
+            _activeUITasks.Remove(taskUI);
+
+            //go out of screen (up)
+            taskUI.MoveUp();
+
+            //Allign other tasks
+            AlignTaskUI();
+        }
+    }
+    public void RemoveFirstUITask()
+    {
+        //go out of screen (up)
+        _activeUITasks[0].MoveUp();
+
+        //remove from list
+        _activeUITasks.RemoveAt(0);
+
+        //Allign other tasks
+        AlignTaskUI();
+        
+    }
+
+    private void AlignTaskUI()
+    {
+        if (_activeUITasks.Count <= 0) return;
+
+        _activeUITasks[0].MoveTo(_firstTaskPoint.position.x);
+        for (int i = 1; i < _activeUITasks.Count; i++)
+        {
+            float x = _firstTaskPoint.position.x;
+            for (int j = i; j > 0; j--)
+            {
+                x += _activeUITasks[j - 1].RectTransform.rect.width + _spaceBetweenTasks;
+            }
+            _activeUITasks[i].MoveTo(x);
+        }
+    }
+}
---
+++ Assets/LARJ/Scripts/UI/TaskUI.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PooledObject), typeof(RectTransform))]
+public class TaskUI : MonoBehaviour, IObjectPoolNotifier
+{
+    [HideInInspector] public ObjectPool TaskUIPool = null;
+    [HideInInspector] public RectTransform RectTransform = null;
+
+    [Header("Attributes")]
+    [SerializeField] private TextMeshProUGUI _titleText = null;
+    [SerializeField] private TextMeshProUGUI _rewardText = null;
+    [SerializeField] private Image _timerImage = null;
+    [HideInInspector] public string TaskTitle = "";
+    [HideInInspector] public string TaskRewardText = "";
+    [HideInInspector] public float TimeToComplete = 0f;
+
+    private Coroutine _lastCoroutine;
+
+    private void Start()
+    {
+        RectTransform = GetComponent<RectTransform>();
+    }
+
+    public void MoveTo(float positionX)
+    {
+        if (_lastCoroutine != null)
+        {
+           StopCoroutine(_lastCoroutine);
+        }
+
+        _lastCoroutine = StartCoroutine(MoveCoroutine(positionX));
+    }
+    private IEnumerator MoveCoroutine(float positionX)
+    {
+        while (transform.position.x > positionX)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(positionX, transform.position.y, transform.position.z), 1000f * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void MoveUp()
+    {
+        if (_lastCoroutine != null)
+        {
+            StopCoroutine(_lastCoroutine);
+        }
+
+        _lastCoroutine = StartCoroutine(MoveUpCoroutine());
+    }
+    private IEnumerator MoveUpCoroutine()
+    {
+        float endY = transform.position.y + 1000f;
+        while (transform.position.y < endY)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, endY, transform.position.z), 1000f * Time.deltaTime);
+            yield return null;
+        }
+        TaskUIPool.ReturnObject(gameObject);
+    }
+
+
+    public void OnCreatedOrDequeuedFromPool(bool created)
+    {
+        if (created)
+        {
+            TaskUIPool = GetComponent<PooledObject>()._pool;
+        }
+
+        _titleText.text = TaskTitle;
+        _rewardText.text = TaskRewardText;
+        _timerImage.fillAmount = 1f;
+
+        StartCoroutine(StartTaskTimerCorutine());
+    }
+
+    public void OnEnqueuedToPool()
+    {
+        
+    }
+
+    private IEnumerator StartTaskTimerCorutine()
+    {
+        float timer = 0f;
+
+        while (timer < TimeToComplete)
+        {
+            timer += Time.deltaTime;
+            _timerImage.fillAmount = (TimeToComplete - timer) / TimeToComplete;
+            yield return null;
+        }
+    }
+}
