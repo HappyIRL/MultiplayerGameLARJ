f92d36956b1d20f864d321047b8a33204e2123e2
Luca <luca.beisser@gmx.de>
2020-10-13 18:14:37 +0200

    Added new Safe & Money

---
+++ Assets/LARJ/Scripts/Interactables/Items/SafeOpener.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeOpener : MonoBehaviour
+{
+    [SerializeField] private Transform _door = null;
+    private bool _doorIsClosed = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        if (_doorIsClosed)
+        {
+            StartCoroutine(OpenDoorCoroutine());
+        }
+    }
+    private void CloseDoor()
+    {
+        StartCoroutine(CloseDoorCoroutine());      
+    }
+
+    private IEnumerator OpenDoorCoroutine()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.up, new Vector3(1f, -1f, 0));
+
+        while (Quaternion.Angle(_door.rotation, targetRotation) > 0.01f)
+        {
+            _door.rotation = Quaternion.RotateTowards(_door.rotation, targetRotation, 3f);
+            yield return null;
+        }
+
+        _doorIsClosed = false;
+    }
+
+    private IEnumerator CloseDoorCoroutine()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.up, new Vector3(0, 1, 0.5f));
+
+        while (Quaternion.Angle(_door.rotation, targetRotation) > 0.01f)
+        {
+            _door.rotation = Quaternion.RotateTowards(_door.rotation, targetRotation, 3f);
+            yield return null;
+        }
+
+        _doorIsClosed = true;
+    }
+}
