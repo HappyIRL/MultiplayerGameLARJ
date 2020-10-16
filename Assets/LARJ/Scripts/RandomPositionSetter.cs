ead3cad9a36e3674228020b6c037a758ff0e1823
Luca <luca.beisser@gmx.de>
2020-10-16 20:43:49 +0200

    Added Random position setter (needs testing))

---
+++ Assets/LARJ/Scripts/RandomPositionSetter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RandomPositionSetter : MonoBehaviour
+{   
+    [Serializable]
+    private struct ChangableObject
+    {
+        public Transform Object;
+        public List<Transform> SpawnPositions;
+    }
+
+    [SerializeField] private List<ChangableObject> _changableObjects = null; 
+
+    void Start()
+    {
+        for (int i = 0; i < _changableObjects.Count; i++)
+        {
+            _changableObjects[i].Object.position = _changableObjects[i].SpawnPositions[UnityEngine.Random.Range(0, _changableObjects[i].SpawnPositions.Count)].position;
+        }
+    }
+}
