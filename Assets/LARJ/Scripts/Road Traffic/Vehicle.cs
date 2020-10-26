3d36710d047be17732a0f2687ba0e506bf7669e4
Luca <luca.beisser@gmx.de>
2020-10-26 22:12:46 +0100

    Simple vehicle traffic system

---
+++ Assets/LARJ/Scripts/Road Traffic/Vehicle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PooledObject))]
+public class Vehicle : MonoBehaviour, IObjectPoolNotifier
+{
+    [HideInInspector] public ObjectPool VehiclePool = null;
+    [SerializeField] private float _speed = 5f;
+
+    private Vector3 _targetPosition;
+
+    public void OnCreatedOrDequeuedFromPool(bool created)
+    {
+        if (created)
+        {
+            VehiclePool = gameObject.GetComponent<PooledObject>()._pool;
+        }
+    }
+
+    public void OnEnqueuedToPool()
+    {
+        
+    }
+    public void StartVehicle()
+    {
+        _targetPosition = transform.position + transform.forward * 50f;
+        StartCoroutine(MoveVehicle());
+    }
+
+    private void ReturnVehicleToPool()
+    {
+        VehiclePool.ReturnObject(gameObject);
+    }
+    private IEnumerator MoveVehicle()
+    {
+        while (Vector3.Distance(transform.position, _targetPosition) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, Time.deltaTime * _speed);
+            yield return null;
+        }
+
+        ReturnVehicleToPool();
+    }
+}
---
+++ Assets/LARJ/Scripts/Road Traffic/VehicleSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour
+{
+    [SerializeField] private float _spawnRate = 1f;
+    [SerializeField] private List<Transform> _spawnPoints = null;
+    [SerializeField] private List<ObjectPool> _vehiclePools = null;
+
+    private float _timeToSpawn;
+    private float _timer;
+
+    void Awake()
+    {
+        _timeToSpawn = 1 / _spawnRate;
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= _timeToSpawn)
+        {
+            _timer = 0f;
+
+            SpawnVehicle();
+        }
+    }
+
+    private void SpawnVehicle()
+    {
+        GameObject obj = _vehiclePools[Random.Range(0, _vehiclePools.Count)].GetObject();
+
+        Transform spawn = _spawnPoints[Random.Range(0, _spawnPoints.Count)];
+        obj.transform.position = spawn.position;
+        obj.transform.rotation = spawn.rotation;
+        obj.transform.SetParent(transform);
+
+        obj.GetComponent<Vehicle>().StartVehicle();
+    }
+}
