cfb1d676d5e93a10d42f33d926e610d0fcdb6501
Alex Michelfelder <Alexandermichelfelder@hotmail.com>
2020-10-27 14:27:43 +0100

    Safe-Cracker idea

---
+++ Assets/LARJ/Scripts/AI Testing/Cracker.cs
@@ -0,0 +1,39 @@
+﻿using Photon.Pun.Demo.Asteroids;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cracker : MonoBehaviour, IObjectPoolNotifier 
+{
+    public ObjectPool crackerPool;
+    public int timeToCrack;
+
+    private int _timer = 0;
+
+    
+    public void OnCreatedOrDequeuedFromPool(bool created)
+    {
+    }
+    public void OnEnqueuedToPool()
+    {
+        StopCoroutine(Start());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            crackerPool.ReturnObject(this.gameObject);
+        }
+    }
+
+    private IEnumerator Start()
+    {
+        while (_timer <= timeToCrack)
+        {
+            yield return new WaitForSeconds(1);
+            _timer++;
+        }
+        // Takes money if complete
+    }
+}
---
+++ Assets/LARJ/Scripts/AI Testing/CrackerSpawner.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackerSpawner : MonoBehaviour
+{
+    [SerializeField] private ObjectPool _crackerPool;
+    [SerializeField] private Transform _spawnPoint;
+    public void SpawnCracker()
+    {        
+        var go = _crackerPool.GetObject();
+        var cracker = go.GetComponent<Cracker>();
+                    
+        go.transform.position = _spawnPoint.position;       
+    }
+}
