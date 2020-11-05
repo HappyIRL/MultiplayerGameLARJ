6f64c4e8a527f52157cb82313ae288b0fa4e18b5
Luca <luca.beisser@gmx.de>
2020-11-05 17:51:24 +0100

    Shoot impact when shoot at other players

---
+++ Assets/LARJ/Scripts/BulletImpact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class BulletImpact : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private Coroutine _forceCoroutine;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    public void AddForce(Vector3 direction, float force)
+    {
+        direction = direction.normalized;
+        Vector3 impact = direction * force;
+        if (_forceCoroutine != null) StopCoroutine(_forceCoroutine);
+        _forceCoroutine = StartCoroutine(AddForceCoroutine(impact));
+    }
+    private IEnumerator AddForceCoroutine(Vector3 impact)
+    {
+        while (impact.magnitude > 0.2f)
+        {
+            _characterController.Move(impact * Time.deltaTime);
+            impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
