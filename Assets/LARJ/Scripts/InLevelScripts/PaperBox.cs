d138f53735534eb8660d7dba5656fe4b59e2fa4a
Donschnitzel <58332117+Donschnitzel@users.noreply.github.com>
2020-11-08 17:57:07 +0100

    Paper Box

---
+++ Assets/LARJ/Scripts/InLevelScripts/PaperBox.cs
@@ -0,0 +1,25 @@
+﻿using cakeslice;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperBox : MonoBehaviour
+{
+     [SerializeField] private Outline _outline;
+    private void Start()
+    {
+        _outline.enabled = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _outline.enabled = true;            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(_outline.enabled)
+        _outline.enabled = false;
+    }
+}
